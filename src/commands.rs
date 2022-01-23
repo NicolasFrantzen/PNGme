b5b384aff2e8d20055c22b319c4cb9fe0fc673ee
Nicolas Bru Frantzen <79478528+NicolasBruFrantzen@users.noreply.github.com>
2022-01-23 13:54:34 +0100

    Encryption (#7)
    * Added optional encryption of hidden message.
    * Added Cargo.lock.
    
    * Added Cargo.lock.

--- src/commands.rs
+++ src/commands.rs
@@ -2,6 +2,7 @@ use crate::args::{DecodeArgs, EncodeArgs, PrintArgs, RemoveArgs};
 use crate::png::Png;
 use crate::chunk::Chunk;
 use crate::chunk_type::ChunkType;
+use crate::encdec;
 
 use anyhow::{Result, bail};
 
@@ -16,7 +17,11 @@ pub fn encode(args: &EncodeArgs) -> Result<()>
     let mut png = Png::try_from(&content[..])?;
 
     let chunk_type = ChunkType::from_str(&args.chunk_type)?;
-    let data = args.message.clone().as_bytes().to_vec();
+
+    let data: Vec<u8> = match &args.encryption_key {
+        Some(key) => encdec::encrypt_message(&key, &args.message).as_bytes().to_vec(),
+        None => args.message.clone().as_bytes().to_vec(),
+    };
 
     if chunk_type.is_valid()
     {
@@ -36,6 +41,7 @@ pub fn encode(args: &EncodeArgs) -> Result<()>
     Ok(())
 }
 
+
 /// Searches for a message hidden in a PNG file and prints the message if one is found
 pub fn decode(args: &DecodeArgs) -> Result<()>
 {
@@ -43,13 +49,22 @@ pub fn decode(args: &DecodeArgs) -> Result<()>
     let png = Png::try_from(&content[..])?;
 
     match png.chunk_by_type(&args.chunk_type) {
-        Some(chunk) =>
-            println!("The secret message is: {chunk_str}", chunk_str = chunk.data_as_string()?),
-        None => println!("Chunk type not found."),
+        Some(chunk) => {
+            let data = chunk.data_as_string()?;
+            let message = match &args.decryption_key {
+                Some(key) => encdec::decrypt_message(key, &data)?,
+                None => data,
+            };
+
+            println!("The secret message is: {message}")
+        }
+            ,
+        None => bail!("Chunk type not found."),
     };
 
     Ok(())
 }
+
 
 /// Removes a chunk from a PNG file and saves the result
 pub fn remove(args: &RemoveArgs) -> Result<()>
@@ -61,6 +76,7 @@ pub fn remove(args: &RemoveArgs) -> Result<()>
 
     Ok(())
 }
+
 
 /// Prints all of the chunks in a PNG file
 pub fn print_chunks(args: &PrintArgs) -> Result<()>
